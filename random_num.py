ea474393a312c5d6bbeef2b56c2df4622ce0a0cb
wildcat8390 <wildcat8390@gmail.com>
2016-02-02 18:29:53 -0600

    FizzBuzz

---
+++ random_num.py
@@ -0,0 +1,11 @@
+def Guess(a):
+    b = Math.floor((Math.random() *50)+1)
+    if a < b:
+        print ('number is bigger')
+    else:
+        print ('number is smaller')
+
+        
+    
+    
+G
